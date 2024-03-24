d592bbead2195d9aaa2edae099f6224cc6203986
UnCompa <brandonddxd@gmail.com>
2024-03-24 12:34:26 -0500

    style[nav] diseño mejorado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavLink, { Icon } from "./NavLink.jsx";
 function NavBar() {
-    const [openMenu, setOpenMenu] = useState(false)
-const handleMenu = () => {
-    setOpenMenu(!openMenu)
-}
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
-    <nav className={`box-border py-2 bg-black shadow-xl text-white flex justify-between items-center sticky top-0 px-8`}>
+    <nav
+      className={`h-28 box-border bg-black shadow-xl text-white flex justify-between items-center sticky top-0 px-12`}
+    >
       <img src="/imgs/UEFPescudoblanco.png" className="h-24" />
-      <ul className={`bg-zinc-900 h-screen fixed top-28 w-1/2 right-0 transition-all ${openMenu ? "translate-x-full" : ""} lg:h-full lg:relative lg:top-0 lg:flex lg:items-center lg:justify-center lg:w-full lg:bg-transparent lg:gap-x-4`}>
+      <ul
+        className={`bg-zinc-900 h-screen fixed top-28 w-1/2 right-0 p-4 flex flex-col gap-y-4 transition-all ${
+          openMenu ? "translate-x-0 lg:translate-x-0" : " translate-x-full lg:translate-x-0"
+        } lg:h-full lg:relative lg:top-0 lg:flex lg:flex-row lg:items-center lg:justify-center lg:w-max lg:bg-transparent lg:gap-x-4`}
+      >
         <li>
-          <NavLink to="/" text={"Inicio"}/>
+          <NavLink to="/" text={"Inicio"} />
         </li>
         <li>
-          <NavLink to="/a" text={"Academicos"}/>
+          <NavLink to="/a" text={"Academicos"} />
         </li>
         <li>
-          <NavLink to="/b" text={"Noticias"}/>
+          <NavLink to="/b" text={"Noticias"} />
         </li>
         <li>
-          <NavLink to="/c" text={"Contacto"}/>
+          <NavLink to="/c" text={"Contacto"} />
         </li>
       </ul>
-      <button onClick={handleMenu} className="text-white fill-white h-6 w-6 block lg:hidden">
+      <button
+        onClick={handleMenu}
+        className="text-white fill-white h-6 w-6 block lg:hidden"
+      >
         <svg
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
