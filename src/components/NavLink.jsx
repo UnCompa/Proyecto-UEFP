c0519bedc0b01b84ea3227cb0ab5ed552a00fbfa
UnCompa1777 <brandonowo26@gmail.com>
2024-03-25 11:58:33 -0500

    feat[seact] se agrego un seccion de mision y vision :D

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -15,7 +15,7 @@ function NavLink({to, text}) {
   return (
     <a
       href={`${to}`}
-      class="rounded-full px-4 py-2 flex items-center gap-x-2 font-Rubik hover:rounded-full hover:ring-2 hover:ring-white hover:bg-white hover:text-black transition-colors duration-200 ease-out"
+      className="rounded-full px-4 py-2 flex items-center gap-x-2 font-Rubik hover:rounded-full hover:ring-2 hover:ring-white hover:bg-white hover:text-black transition-colors duration-200 ease-out"
     >
         {text}
       <span>
