9a06cb066eb4d6b1ca6a0e9402f00f03c4d0a05a
UnCompa <brandonddxd@gmail.com>
2024-03-26 06:11:40 -0500

    No se logro intergrar el video aun

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,11 @@
+import {ReactPlayer} from "react-player";
+
+export const VideoPlayer = () => {
+  return (
+    <section class="h-screen w-full grid place-content-center container mx-auto">
+      <div>
+        <video src="" controls></video>
+      </div>
+    </section>
+  );
+};
