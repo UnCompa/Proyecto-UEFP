100fce5f2de9e118c2eefb59b827b7a5aba391eb
santiagoMosquera99 <krakedev3@gmail.com>
2024-04-15 21:31:21 -0500

    style[dashh]Mejoras en dashboard

--- src/components/dashboard/MainDashboard.jsx
+++ src/components/dashboard/MainDashboard.jsx
@@ -1,46 +1,108 @@
 import { useEffect } from "react";
-import FormTest from "./FormTest";
 import { supabase } from "../../lib/supabase";
+import { FaAddressBook, FaEnvelope, FaGraduationCap, FaHome, FaUsers } from "react-icons/fa";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 
-export default function MainDashboard({email}) {
-  useEffect(()=>{
-    const fetchData = async () =>{
-      const result = await supabase.from('pages').select()
+export default function MainDashboard({ email }) {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await supabase.from("pages").select();
       console.log(result);
-    }
-    fetchData()
-  },[])
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <h1 className="font-bold text-4xl">👋 Bienvenido,<span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-red-500"> {email}</span></h1>
-      <p className="text-xl py-2">Estamos felices de verte aquí</p>
-      <main className="bg-zinc-900 my-4 rounded">
-        <section className="grid grid-cols-2 py-4 place-content-center">
+      <h1 className="font-bold text-2xl md:text-4xl">
+        👋 Bienvenido,
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-red-500">
+          {" "}
+          {email}
+        </span>
+      </h1>
+      <p className="text-base md:text-xl py-2">Estamos felices de verte aquí</p>
+      <main className="my-4 rounded flex flex-col gap-y-4">
+        <section className="bg-zinc-200 dark:bg-zinc-900 grid grid-cols-1 md:grid-cols-2 py-4 place-content-center">
           <div className="p-8 flex flex-col justify-center">
-            <h2 className="text-5xl font-bold py-4">Empieza a crear y editar nuestra pagina</h2>
-            <p className="text-lg">Aqui podras tener libertade de cambiar y subir contenido a la plataforma para mantenerla en funcionamiento y actualizada</p>
+            <h2 className="text-5xl font-bold py-4">
+              Empieza a crear y editar nuestra pagina
+            </h2>
+            <p className="text-lg text-black dark:text-zinc-300">
+              Aqui podras tener libertade de cambiar y subir contenido a la
+              plataforma para mantenerla en funcionamiento y actualizada
+            </p>
           </div>
           <div className="flex items-center justify-center">
-            <img className="h-96" src="/public/svg/calendar-55.svg" alt="" />
+            <img className="h-96" src="/svg/calendar-55.svg" alt="" />
           </div>
         </section>
-        <section className="grid grid-cols-2 py-4 place-content-center">
-          <div className="p-8 flex flex-col justify-center">
-            <h2 className="text-5xl font-bold py-4">Empieza a crear y editar nuestra pagina</h2>
-            <p className="text-lg">Aqui podras tener libertade de cambiar y subir contenido a la plataforma para mantenerla en funcionamiento y actualizada</p>
-          </div>
-          <div className="flex items-center justify-center">
-            <img className="h-96" src="/public/svg/calendar-55.svg" alt="" />
-          </div>
-        </section>
-        <section className="grid grid-cols-2 py-4 place-content-center">
-          <div className="p-8 flex flex-col justify-center">
-            <h2 className="text-5xl font-bold py-4">Empieza a crear y editar nuestra pagina</h2>
-            <p className="text-lg">Aqui podras tener libertade de cambiar y subir contenido a la plataforma para mantenerla en funcionamiento y actualizada</p>
-          </div>
-          <div className="flex items-center justify-center">
-            <img className="h-96" src="/public/svg/calendar-55.svg" alt="" />
-          </div>
+        <section className="my-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <article className="bg-zinc-200 dark:bg-zinc-900 rounded hover:ring-1 hover:ring-green-500 h-full w-full p-4 grid grid-cols-2 transition-all">
+            <div className="h-full w-full">
+              <h3 className="text-center font-bold text-4xl py-2 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-green-300">
+                Inicio
+              </h3>
+              <p className="font-light dark:text-zinc-300 my-2">
+                Edita la pagina principal ya sea el lema, la mision y vision de
+                la institucion, entre otras cosas
+              </p>
+              <a href="">
+                <button className="my-2 px-4 py-1 rounded hover:bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-zinc-900 border border-green-500 transition-all">Ver Mas</button>
+              </a>
+            </div>
+            <div className="h-full w-full grid place-content-center">
+              {/* <img src="" alt="Ilustracion del inicio" /> */}
+              <FaHome className="text-9xl fill-green-500 drop-shadow-2xl"/>
+            </div>
+          </article>
+          <article className="bg-zinc-200 dark:bg-zinc-900 rounded hover:ring-1 hover:ring-purple-500 h-full w-full p-4 grid grid-cols-2 transition-all">
+            <div className="h-full w-full">
+              <h3 className="text-center font-bold text-4xl py-2 bg-clip-text text-transparent bg-gradient-to-tl from-purple-700 to-purple-300">
+                Sobre Nosotros
+              </h3>
+              <p className="font-light dark:text-zinc-300 my-2">
+                Todo sobre la historia de la institucion, agrega y modifica esta seccion a traves de paneles de ayuda
+              </p>
+              <a href="">
+                <button className="my-2 px-4 py-1 rounded hover:bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-zinc-900 border border-purple-500 transition-all">Ver Mas</button>
+              </a>
+            </div>
+            <div className="h-full w-full grid place-content-center">
+            <FaUsers className="text-9xl fill-purple-500 drop-shadow-2xl"/>
+            </div>
+          </article>
+          <article className="bg-zinc-200 dark:bg-zinc-900 rounded hover:ring-1 hover:ring-orange-500 h-full w-full p-4 grid grid-cols-2 transition-all">
+            <div className="h-full w-full">
+              <h3 className="text-center font-bold text-4xl py-2 bg-clip-text text-transparent bg-gradient-to-r from-orange-700 to-orange-300">
+                Academicos
+              </h3>
+              <p className="font-light dark:text-zinc-300 my-2">
+                Actualiza el contenido sobre las promociones que se ofrece a traves de este panel
+              </p>
+              <a href="">
+                <button className="my-2 px-4 py-1 rounded hover:bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-zinc-900 border border-orange-500 transition-all">Ver Mas</button>
+              </a>
+            </div>
+            <div className="h-full w-full grid place-content-center">
+            <FaAddressBook className="text-9xl fill-orange-500 drop-shadow-2xl"/>
+            </div>
+          </article>
+          <article className="bg-zinc-200 dark:bg-zinc-900 rounded hover:ring-1 hover:ring-red-500 h-full w-full p-4 grid grid-cols-2 transition-all">
+            <div className="h-full w-full">
+              <h3 className="text-center font-bold text-4xl py-2 bg-clip-text text-transparent bg-gradient-to-tl from-red-700 to-red-300">
+                Contactos
+              </h3>
+              <p className="font-light dark:text-zinc-300 my-2">
+                Todo sobre la historia de la institucion, agrega y modifica esta seccion a traves de paneles de ayuda
+              </p>
+              <a href="">
+                <button className="my-2 px-4 py-1 rounded hover:bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-zinc-900 border border-red-500 transition-all">Ver Mas</button>
+              </a>
+            </div>
+            <div className="h-full w-full grid place-content-center">
+              <MdOutlineAlternateEmail className="text-9xl fill-red-500 drop-shadow-2xl"/>
+            </div>
+          </article>
         </section>
       </main>
     </>
