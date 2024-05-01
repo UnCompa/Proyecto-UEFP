a4434ab4d847a67de59d6d6cb0125edfed3e88c7
UnCompa1777 <brandonowo26@gmail.com>
2024-05-01 09:58:24 -0500

    style[contacts] se mejoro el ui

--- src/components/sections/ContacsForm.jsx
+++ src/components/sections/ContacsForm.jsx
@@ -1,30 +1,35 @@
 import { Input, Textarea } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 import { getLangFromUrl } from "../../i18n/utils";
-export default function ContacsForm({ urlStr,url }) {
-  const lang = getLangFromUrl(url)
+import { RxCrossCircled } from "react-icons/rx";
+import { motion } from "framer-motion";
+export default function ContacsForm({ urlStr, url }) {
+  const lang = getLangFromUrl(url);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  console.log(errors);
   const onSubmit = handleSubmit(async (data) => {
     console.log("Hola");
     console.log(data);
     console.log(errors);
-    const formData = new FormData()
-    formData.append("email", data.email)
-    formData.append("asunto", data.asunto)
-    formData.append("mensaje", data.mensaje)
+    const formData = new FormData();
+    formData.append("email", data.email);
+    formData.append("asunto", data.asunto);
+    formData.append("mensaje", data.mensaje);
     console.log(formData);
-    const res = await fetch("https://formsubmit.co/1115e155c3f8b6719a8318cc356e33c6 ", {
-      method: "POST",
-      body: formData,
-      headers: {
-        "Access-Control-Allow-Origin": "https://unidad-educativa-fiscal-pichincha.vercel.app",
+    const res = await fetch(
+      "https://formsubmit.co/1115e155c3f8b6719a8318cc356e33c6 ",
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Access-Control-Allow-Origin":
+            "https://unidad-educativa-fiscal-pichincha.vercel.app",
+        },
       }
-    })
+    );
     console.log(res);
   });
   return (
@@ -32,11 +37,20 @@ export default function ContacsForm({ urlStr,url }) {
       className="flex flex-col gap-y-2 font-Rubik w-full"
       onSubmit={onSubmit}
     >
-      <label htmlFor="email"> Correo Electronico </label>
+      <motion.label
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ transition: 0.1, ease: "easeOut" }}
+        htmlFor="email"
+      >
+        Correo Electronico
+      </motion.label>
+      <div className="bg-gradient-to-r to-red-500/30 from-pink-500 h-0.5 w-1/2 rounded"></div>
       <Input
         type="email"
-        variant="underlined"
-        color="default"
+        variant="bordered"
+        color="danger"
         placeholder="tuemail@gmail.com"
         name="email"
         {...register("email", {
@@ -51,13 +65,27 @@ export default function ContacsForm({ urlStr,url }) {
         })}
       />
       {errors.email && (
-        <span className="py-2 text-red-500 italic">{errors.email.message}</span>
+        <div className="flex items-center gap-x-2">
+          <RxCrossCircled className="text-red-500" />
+          <span className="font-Rubik text-xs py-1 text-red-500 italic">
+            {errors.email.message}
+          </span>
+        </div>
       )}
-      <label htmlFor="Asunto"> Asunto </label>
+      <motion.label
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ transition: 0.1, ease: "easeOut" }}
+        htmlFor="Asunto"
+      >
+        Asunto
+      </motion.label>
+      <div className="bg-gradient-to-r to-red-500/30 from-pink-500 h-0.5 w-1/2 rounded"></div>
       <Input
         type="text"
-        variant="underlined"
-        color="default"
+        variant="bordered"
+        color="danger"
         placeholder="Ej: Solicitud de informacion"
         name="Asunto"
         {...register("asunto", {
@@ -67,23 +95,35 @@ export default function ContacsForm({ urlStr,url }) {
           },
           minLength: {
             value: 5,
-            message: "El asunto debe ser de minimo 5 caracteres"
+            message: "El asunto debe ser de minimo 5 caracteres",
           },
           maxLength: {
             value: 42,
-            message: "El asunto maximo es de 42 caracteres"
-          }
+            message: "El asunto maximo es de 42 caracteres",
+          },
         })}
       />
       {errors.asunto && (
-        <span className="py-2 text-red-500 italic">
-          {errors.asunto.message}
-        </span>
+        <div className="flex items-center gap-x-2">
+          <RxCrossCircled className="text-red-500" />
+          <span className="font-Rubik text-xs py-1 text-red-500 italic">
+            {errors.asunto.message}
+          </span>
+        </div>
       )}
-      <label htmlFor="Contenido"> Mensaje </label>
+      <motion.label
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ transition: 0.1, ease: "easeOut" }}
+        htmlFor="Contenido"
+      >
+        Mensaje
+      </motion.label>
+      <div className="bg-gradient-to-r to-red-500/30 from-pink-500 h-0.5 w-1/2 rounded"></div>
       <Textarea
-        color="default"
-        variant="underlined"
+        color="danger"
+        variant="bordered"
         placeholder="Ej: Buenas tardes deseo..."
         name="Contenido"
         {...register("mensaje", {
@@ -93,23 +133,31 @@ export default function ContacsForm({ urlStr,url }) {
           },
           minLength: {
             value: 24,
-            message: "El mensaje debe ser de minimo 24 caracteres"
+            message: "El mensaje debe ser de minimo 24 caracteres",
           },
           maxLength: {
             value: 320,
-            message: "El asunto maximo es de 320 caracteres"
-          }
+            message: "El asunto maximo es de 320 caracteres",
+          },
         })}
       />
       {errors.mensaje && (
-        <span className="py-2 text-red-500 italic">
-          {errors.mensaje.message}
-        </span>
+        <div className="flex items-center gap-x-2">
+          <RxCrossCircled className="text-red-500" />
+          <span className="font-Rubik text-xs py-1 text-red-500 italic">
+            {errors.mensaje.message}
+          </span>
+        </div>
       )}
       <input type="hidden" name="_next" value={urlStr} />
       <input type="hidden" name="_captcha" value="false" />
-      <input type="hidden" name="_template" value="table"/>
-      <button type="submit">Enviar informacion</button>
+      <input type="hidden" name="_template" value="table" />
+      <button
+        type="submit"
+        className="bg-red-500 py-2 rounded my-2 shadow-lg dark:shadow-red-500/30"
+      >
+        Enviar información
+      </button>
     </form>
   );
 }
