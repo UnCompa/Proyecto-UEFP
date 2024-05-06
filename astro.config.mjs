e97b242ba18b45ead3aea39c117ab5a28cb8ce5e
santiagoMosquera99 <krakedev3@gmail.com>
2024-05-05 20:55:46 -0500

    config[astro] se agrego la analiticas otra ves

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -8,25 +8,30 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap({
-    i18n: {
-      defaultLocale: 'es',
-      // Todas las URL que no contengan `es` o `fr` después de `https://stargazers.club/` se tratarán como el idioma predeterminado, es decir, `en`
-      locales: {
-        en: 'en-US',
-        // El valor de `defaultLocale` debe estar presente en las claves de `locales`
-        es: 'es-ES'
-      }
-    }
-  }), mdx()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        // Todas las URL que no contengan `es` o `fr` después de `https://stargazers.club/` se tratarán como el idioma predeterminado, es decir, `en`
+        locales: {
+          en: "en-US",
+          // El valor de `defaultLocale` debe estar presente en las claves de `locales`
+          es: "es-ES",
+        },
+      },
+    }),
+    mdx(),
+  ],
   site: "https://unidad-educativa-fiscal-pichincha.vercel.app",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
     routing: {
-      prefixDefaultLocale: true
-    }
+      prefixDefaultLocale: true,
+    },
   },
   output: "server",
-  adapter: vercel()
+  adapter: vercel({ webAnalytics: true }),
 });
